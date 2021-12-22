92c29133775cbcf82ed697ebdc6f542066a44dfd
Daan <daanreijnders@outlook.com>
2021-12-22 12:44:21 +0100

    processed reviewer comments

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This repository holds the data generation and analysis scripts for the manuscript 'Simulating Lagrangian Subgrid-Scale Dispersion on Neutral Surfaces in the Ocean'. 
 
 ## `ACC_mitgcm_config`
-Contains MITgcm configuration files for the coarse and fine model, including spinups. This includes the compilation code and namelists.
+Contains MITgcm configuration files for the coarse and fine model, including their spinups. This includes the compilation code and namelists.
 
 ## `ACC_particle_experiments`
 Contains Python scripts for the Lagrangian simulations carried out with [Parcels](https://github.com/OceanParcels/parcels) (experiments with a tracer patch, and with particles initialized on a lattice), and analysis scripts pertaining to these simulations and their output.
@@ -16,6 +16,9 @@ Experiments using the idealized set-up.
 ## `kernels`
 Functions used by [Parcels](https://github.com/OceanParcels/parcels) to drive the Markov models.
 
+## `misc`
+Contains a PDF copy of the documentation for MITgcm's ACC channel simulation. At the date of writing, this documentation is also available online at https://mitgcm.readthedocs.io/en/latest/examples/reentrant_channel/reentrant_channel.html.
+
 ## `tools`
 Miscellaneous helper functions.
 
@@ -23,5 +26,6 @@ Miscellaneous helper functions.
 Notebooks that inspect the MITgcm model output.
 
 ## `visualization`
-For some of the figures in the manuscript. Others are located under the `ACC_particle_experiments` > `analysis` notebooks.
+For some of the figures in the manuscript. Others are located under the `ACC_particle_experiments` > `analysis` notebooks, or under `idealized_experiments/3D_3D_idealized_isopycnal_Markov1.ipynb`
+
 
---
+++ idealized_experiments/diapycnal_diffusivity_compare.py
@@ -0,0 +1,296 @@
+import math
+import random
+import numpy as np
+import xarray as xr
+
+from datetime import timedelta
+import sys
+import warnings
+
+
+import parcels
+from parcels import (
+    AdvectionDiffusionEM,
+    AdvectionRK4_3D,
+    AdvectionDiffusionM1,
+    DiffusionUniformKh,
+    FieldSet,
+    JITParticle,
+    ScipyParticle,
+    ParticleSet,
+    Variable,
+    ParcelsRandom,
+)
+import importlib
+import pickle
+import pandas as pd
+from scipy.integrate import dblquad
+
+sys.path.append("/Users/4302001/surfdrive/diffusion-hydro-mod/kernels")
+sys.path.append("/nethome/4302001/diffusion-hydro-mod/kernels")
+import idealized_isopycnal_field
+import elements_analytical
+import K_Le_Sommer
+import K_Redi
+import K_Redi_smallslope
+import EM_3D_BC
+import M1_3D_BC
+import idealized_isopycnal_field
+import Markov1_3D_BC_taper
+import Markov1_3D_BC_taper_init
+importlib.reload(idealized_isopycnal_field)
+
+def create_analytical_fieldset(
+    rhoField,
+    Ki=1000,
+    Tl=1 * 24 * 60 * 60,
+    nu_squared=None,  # RMS eddy velocity, following Koszalka
+    epsilon=None,
+    eta=None,
+    expansion_terms=None,
+):
+    """
+    Prepares a fieldset of an idealized isopycnal field.
+
+    The fieldset can be used for numerically computing trajectories using Markov-1 or Markov-0 (diffusive) parameterizations. 
+    Note that in case of Markov-1, `nu_squared` is computed using the assymptotic behavior: Ki = nu_squared * Tl,
+    so only `Ki` and `Tl` need to be specified. 
+    """
+    
+    data = {
+        "U": np.zeros(1),
+        "V": np.zeros(1),
+        "W": np.zeros(1),
+        "boundaryMask": np.ones(1),
+    }
+    
+    dims = {"lon": 1, "lat": 1, "depth": 1}
+
+    fieldset = parcels.FieldSet.from_data(
+        data,
+        dims,
+        mesh="flat",
+        allow_time_extrapolation=True,
+    )
+
+    # For analytical solution
+    fieldset.add_constant("gval", rhoField.g)
+    fieldset.add_constant("Nsquaredval", rhoField.Nsquared)
+    fieldset.add_constant("alphaxval", rhoField.alphax)
+    fieldset.add_constant("alphayval", rhoField.alphay)
+    fieldset.add_constant("kappaxval", rhoField.kappax)
+    fieldset.add_constant("kappayval", rhoField.kappay)
+    fieldset.add_constant("rho0val", rhoField.rho0)
+
+    # For Milstein
+    if expansion_terms:
+        fieldset.add_constant("expansion_terms", expansion_terms)
+
+    # For Markov-0
+    fieldset.add_constant("kappaval", Ki)
+
+    # For Markov-1
+    fieldset.add_constant("TL", Tl)  # Lagrangian timescale
+    if Ki:
+        fieldset.add_constant("nusquared", Ki / Tl)
+    else:
+        fieldset.add_constant("nusquared", nu_squared)
+
+    if epsilon:
+        fieldset.add_constant("epsilon", epsilon)
+    else:
+        fieldset.add_constant("epsilon", 0)
+        
+    if eta:
+        fieldset.add_constant("etaval", eta)
+
+    return fieldset
+
+class Markov1Particle(parcels.JITParticle):
+    u_prime = parcels.Variable('u_prime', initial=0)
+    v_prime = parcels.Variable('v_prime', initial=0)
+    w_prime = parcels.Variable('w_prime', initial=0)
+    
+def get_test_particles(
+    rhoField, fieldset, rho=1027.5, nx=25, ny=50, xbounds=(250000, 750000), ybounds=(250000, 1750000), pclass=Markov1Particle
+):
+    XX, YY = np.meshgrid(
+        np.linspace(xbounds[0], xbounds[1], nx), np.linspace(ybounds[0], ybounds[1], ny), indexing='ij'
+    )
+    lons = XX.flatten()
+    lats = YY.flatten()
+    depth = rhoField.isopycnal_array(lons, lats, rho_iso=rho)
+    pset = parcels.ParticleSet.from_list(
+        fieldset,
+        pclass=pclass,
+        lon=lons,
+        lat=lats,
+        depth=depth,
+        time=np.zeros(nx * ny),
+        lonlatdepth_dtype=np.float64,
+    )
+
+    return pset
+
+def get_test_particles_dense(
+    rhoField, fieldset, rho=1027.5, nx=80, ny=160, xbounds=(0, 1_000_000), ybounds=(0, 2_000_000), pclass=Markov1Particle
+):
+    """Get a set of evenly spaced testparticles, specified within xbounds and ybounds"""
+    
+    x_shift = (xbounds[1]-xbounds[0])/nx/2
+    y_shift = (ybounds[1]-ybounds[0])/ny/2
+    
+    if x_shift != y_shift:
+        warnings.warn('dx and dy not the same')
+    
+    XX, YY = np.meshgrid(
+        np.linspace(xbounds[0] + x_shift, xbounds[1] - x_shift, nx), np.linspace(ybounds[0] + y_shift, ybounds[1] - y_shift, ny), indexing='ij'
+    )
+    lons = XX.flatten()
+    lats = YY.flatten()
+    depth = rhoField.isopycnal_array(lons, lats, rho_iso=rho)
+    pset = parcels.ParticleSet.from_list(
+        fieldset,
+        pclass=pclass,
+        lon=lons,
+        lat=lats,
+        depth=depth,
+        time=np.zeros(nx * ny),
+        lonlatdepth_dtype=np.float64,
+    )
+
+    return pset
+
+def rms_z_error(x, y, z, rho=1027.5):
+    """Determines the error (average distance from the isopycnal)"""
+    
+    true = myField.isopycnal_array(x, y, rho)
+    rms_error = np.sqrt(np.mean((z - true)**2))
+    return rms_error
+
+def diapycnal_flux(x, y, z, t, rho=1027.5, tmode="seconds"):
+    """Determines the diapycnal flux over time"""
+    
+    rms_error = rms_z_error(x, y, z, rho)
+    
+    if tmode == "hours":
+        multip = 60 * 60
+    elif tmode == "seconds":
+        multip = 1
+    return 0.5 * rms_error**2 / (multip * t)
+
+def plotTestParticles(testParticles):
+    fig = go.Figure(data=[go.Surface(x=XX, y=YY, z=ZZ_iso1027, opacity=0.4),
+                          go.Scatter3d(x=testParticles.lon, y=testParticles.lat, z=testParticles.depth,
+                                       mode='markers', marker=dict(size=1, color='blue'), opacity=0.8)])
+    fig.update_layout(
+        title="Isopycnal",
+        autosize=False,
+        width=1200,
+        height=600,
+        margin=dict(l=65, r=50, b=65, t=90),
+        scene=dict(
+            aspectratio=dict(x=1, y=2, z=0.5),
+            xaxis=dict(title="x [km]"),
+            yaxis=dict(title="y [km]"),
+            zaxis=dict(title="z [m]", nticks=3),
+        ),
+    )
+
+    fig.show()
+    
+def deleteParticle(particle, fieldset, time):
+    particle.delete()
+    
+def periodic_BC(particle, fieldset, time):
+    if particle.lon > 1_000_000:
+        particle.lon -= 1_000_000
+    elif particle.lon < 0:
+        particle.lon += 1_000_000
+    if particle.lat > 2_000_000:
+        particle.lat -= 2_000_000
+    elif particle.lat < 0:
+        particle.lat += 2_000_000
+        
+        
+myField = idealized_isopycnal_field.densityField()
+myField.create_interpolated_density_grid(nx=201, ny=401, nz=901, H=6000)
+
+ZZ_iso1027, XX, YY = myField.isopycnal_grid(1027.5, myField.X, myField.Y)
+
+fieldset = create_analytical_fieldset(myField, Ki=1000)
+
+def add_dummy_settings(fieldset):
+    fieldset.add_constant("upperBound", 1e9)
+    fieldset.add_constant("lowerBound", -1e9)
+    fieldset.add_constant("northBound", 1e9)
+    fieldset.add_constant("southBound", -1e9)
+    fieldset.add_constant("Sc", 1)
+    fieldset.add_constant("Sd", 0.001)
+    
+dt_mins = np.array([10, 20, 40, 80, 160, 360, 720, 1440])
+runtypes = ["redi_full", "redi_smallslope", "redi_full_M1", "markov-1"]
+diapycnal_diffusivities = pd.DataFrame(columns=runtypes, index=dt_mins)
+
+for run in runtypes:
+    if run == "redi_full":
+        fieldset = create_analytical_fieldset(myField, Ki=1000, epsilon=0)
+        add_dummy_settings(fieldset)
+    elif run == "redi_smallslope":
+        fieldset = create_analytical_fieldset(myField, Ki=1000, epsilon=0)
+        add_dummy_settings(fieldset)
+    elif run == "redi_full_M1":
+        fieldset = create_analytical_fieldset(myField, Ki=1000, expansion_terms=8, epsilon=1e-13)
+        add_dummy_settings(fieldset)
+    for dt in dt_mins:
+        if run == "markov-1":
+            fieldset = create_analytical_fieldset(myField, Ki=1000, Tl = 20 * 24 * 60 * 60, epsilon=int(dt) * 60 / (20 * 24 * 60 * 60), eta=1e-8)
+            add_dummy_settings(fieldset)
+        print(f"Running {run} at dt={dt} minutes")
+        testParticles = get_test_particles_dense(myField, fieldset, rho=1027.5)
+        if run == "redi_full":
+            ParcelsRandom.seed(1636)
+            testParticles.execute(testParticles.Kernel(elements_analytical.elements_analytical) \
+                                + testParticles.Kernel(K_Redi.K_Redi) \
+                                + testParticles.Kernel(EM_3D_BC.EM_3D_BC),
+                                  runtime=timedelta(days=90),
+                                  dt=timedelta(minutes=int(dt)),
+                                  verbose_progress=True,
+            )
+        elif run == "redi_smallslope":
+            ParcelsRandom.seed(1636)
+            testParticles.execute(testParticles.Kernel(elements_analytical.elements_analytical) \
+                                + testParticles.Kernel(K_Redi_smallslope.K_Redi_smallslope) \
+                                + testParticles.Kernel(EM_3D_BC.EM_3D_BC),
+                                  runtime=timedelta(days=90),
+                                  dt=timedelta(minutes=int(dt)),
+                                  verbose_progress=True,
+            )
+        elif run == "redi_full_M1":
+            ParcelsRandom.seed(1636)
+            testParticles.execute(testParticles.Kernel(elements_analytical.elements_analytical) \
+                                + testParticles.Kernel(K_Redi.K_Redi)\
+                                + testParticles.Kernel(M1_3D_BC.M1_3D_BC),
+                                  runtime=timedelta(days=90),
+                                  dt=timedelta(minutes=int(dt)),
+                                  verbose_progress=True,
+            )              
+        elif run == "markov-1":
+            ParcelsRandom.seed(1636)
+            testParticles.execute(
+                testParticles.Kernel(elements_analytical.elements_analytical) 
+                + testParticles.Kernel(Markov1_3D_BC_taper_init.Markov1_3D_BC_taper_init),
+                dt=0,
+                verbose_progress=True,
+            )
+            testParticles.execute(testParticles.Kernel(elements_analytical.elements_analytical) \
+                                + testParticles.Kernel(Markov1_3D_BC_taper.Markov1_3D_BC_taper),
+                                  runtime=timedelta(days=90),
+                                  dt=timedelta(minutes=int(dt)),
+                                  verbose_progress=True,
+            )
+        dia_error = diapycnal_flux(testParticles.lon, testParticles.lat, testParticles.depth, 90*24*60*60)
+        diapycnal_diffusivities[run][int(dt)] = dia_error
+        print(dia_error)
+        
+diapycnal_diffusivities.to_csv("diapycnal_diffusivities_90d.csv")
